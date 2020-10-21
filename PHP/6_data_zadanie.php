243277ed75aba6d50d978e1882fbefbd9996931c
PrzybylaHubert <70707087+PrzybylaHubert@users.noreply.github.com>
2020-10-21 21:14:06 +0200

    Update 6_data_zadanie.php

--- PHP/6_data_zadanie.php
+++ PHP/6_data_zadanie.php
@@ -6,7 +6,6 @@
   </head>
   <body>
     <form method="get" action="./6_data_show_zadanie.php">
-
       Name <input type="text" name="name"><br>
       Sex <input type="radio" name="plec" value="m"> Male
       <input type="radio" name="plec" value="f"> Female<br>
@@ -21,6 +20,5 @@
         Describe your athletic ability:<br> <textarea name="textarea" rows="5" cols="30"></textarea><br>
         <input type="submit" value="send">
       </form>
-
   </body>
 </html>
